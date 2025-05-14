e7b9ee8d68df82943d2343d00faf2763a5e2df75
“JihaR15 <jiharamdhan97@gmail.com>
2025-05-14 09:01:50 +0700

    Update: AuthorizeUser

--- SIMANTAP/app/Http/Middleware/AuthorizeUser.php
+++ SIMANTAP/app/Http/Middleware/AuthorizeUser.php
@@ -13,12 +13,14 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
         $user = $request->user();
 
-        if ($user->hasRole($role)) {
-            return $next($request);
+        foreach ($roles as $role) {
+            if ($user->hasRole($role)) {
+                return $next($request);
+            }
         }
         return redirect('/')->with('error', 'Unauthorized access');
     }
